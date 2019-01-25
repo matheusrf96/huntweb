15bc3c63e5d62cf4f88b35d1f31c5d3e59afbb26
Matheus Rodrigues <matheusrf96@gmail.com>
2019-01-25 13:57:53 -0300

    Nova página principal

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 
+import './style.css';
+
 export default class Main extends Component {
+    state = {
+        products: [],
+    }
+
     componentDidMount(){
         this.loadProducts();
     }
@@ -9,10 +15,22 @@ export default class Main extends Component {
     loadProducts = async () => {
         const response = await api.get('/products');
 
-        console.log(response.data.docs);
+        this.setState({products: response.data.docs});
     };
 
     render(){
-        return <h1>Hello</h1>;
+        const { products } = this.state;
+
+        return (
+            <div className="product-list">
+                {products.map(item => (
+                    <article key={item._id}>
+                        <strong>{item.title}</strong>
+                        <p>{item.description}</p>
+                        <a href='{item.url}'>Acessar</a>
+                    </article>
+                ))}
+            </div>
+        );
     }
 }
